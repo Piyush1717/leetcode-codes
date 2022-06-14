bf1fcfef7e4e0560ae46565e58bc7fc19ca0194b
PIYUSH SINGH <96883232+Piyush1717@users.noreply.github.com>
2022-06-14 16:59:29 +0530

    Added solution - LeetHub

---
+++ Duplicate subtree in Binary Tree - GFG/duplicate-subtree-in-binary-tree.java
@@ -0,0 +1,165 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+
+        while(t > 0){
+            String s = br.readLine();
+	    	Node root = buildTree(s);
+	    	
+            Solution ob = new Solution();
+    		System.out.println(ob.dupSub(root));
+            t--;
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* A Binary Tree node
+
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ */
+
+class Solution {
+    HashMap<String,Integer>hm=new HashMap<>();
+    
+    public String solve(Node root)
+    {
+        if(root==null)
+        {
+            return "$";
+        }
+        String s="";
+        if(root.left==null && root.right==null)
+        {
+            s=String.valueOf(root.data);
+            return s;
+        }
+        s=s+String.valueOf(root.data);
+        s=s+solve(root.left);
+            s=s+solve(root.right);
+            if(hm.containsKey(s)==false)
+            {
+                hm.put(s,1);
+            }
+            else{
+                int x=hm.get(s);
+                hm.put(s,x+1);
+            }
+            
+            return s;
+        
+    }
+    
+    int dupSub(Node root) {
+        solve(root);
+        for(String key:hm.keySet())
+        {
+            if(hm.get(key)>=2)
+            {
+                return 1;
+            }
+        }
+        
+        return 0;
+        
+    }
+}
