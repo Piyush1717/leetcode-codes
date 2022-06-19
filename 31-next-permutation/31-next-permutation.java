068ad1210fde2f1c9dbade6f0eeca58880910db8
PIYUSH SINGH <96883232+Piyush1717@users.noreply.github.com>
2022-06-19 15:46:44 +0530

    Time: 2 ms (22.78%), Space: 44.1 MB (21.17%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -2,49 +2,48 @@ class Solution {
 
     
     public void nextPermutation(int[] nums) {
-        
+     
         int n=nums.length;
-        
-        if(nums.length==1)
+        if(n==1)
         {
             return;
         }
         
-        
+        int lastPeak=-1;
         int i=1;
-        int lastInd=-1;
-        
         while(i<n)
         {
             if(nums[i]>nums[i-1])
             {
-                lastInd=i;
+                lastPeak=i;
             }
             i++;
         }
         
-        if(lastInd==-1)
+        if(lastPeak==-1)
         {
             Arrays.sort(nums);
             return;
         }
         
-        int index=lastInd;
-        for(int j=lastInd+1;j<n;j++)
+        int index=lastPeak;
+        int val=nums[lastPeak];
+        
+        for(int j=lastPeak+1;j<n;j++)
         {
-            if(nums[j]>nums[lastInd-1] && nums[j]<nums[index])
+            if(nums[j]>nums[lastPeak-1] && nums[j]<nums[index])
             {
                 index=j;
             }
         }
         
-        // swap(nums,index,lastIdx-1);
-        
+        //swap
         int temp=nums[index];
-        nums[index]=nums[lastInd-1];
-        nums[lastInd-1]=temp;
+        nums[index]=nums[lastPeak-1];
+        nums[lastPeak-1]=temp;
         
-        Arrays.sort(nums,lastInd,n);
+        Arrays.sort(nums,lastPeak,n);
+        
    
     }
 }
