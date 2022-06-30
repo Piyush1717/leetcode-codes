b51a14f51eb8d3ab05830cedfdcd51d785f33d98
PIYUSH SINGH <96883232+Piyush1717@users.noreply.github.com>
2022-06-30 19:36:19 +0530

    Added solution - LeetHub

---
+++ Mother Vertex - GFG/mother-vertex.java
@@ -0,0 +1,77 @@
+// { Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+            for(int i = 0; i < V; i++)
+                adj.add(i, new ArrayList<Integer>());
+            for(int i = 0; i < E; i++){
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+            }
+            Solution obj = new Solution();
+            int ans = obj.findMotherVertex(V, adj);
+            System.out.println(ans);
+       }
+    }
+}// } Driver Code Ends
+
+
+
+class Solution
+{
+    //Function to find a Mother Vertex in the Graph.
+    public int findMotherVertex(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        int mv=0;
+        boolean [] vis=new boolean[V];
+        
+        for(int i=0;i<V;i++)
+        {
+            if(vis[i]==false)
+            {
+                dfs(i,adj,vis);
+               mv=i; 
+            }
+        }
+        
+        Arrays.fill(vis,false);
+        
+        dfs(mv,adj,vis);
+        for(int i=0;i<V;i++)
+        {
+            if(vis[i]==false)
+            {
+                return -1;
+            }
+        }
+        return mv;
+    }
+    
+    public void dfs(int v,ArrayList<ArrayList<Integer>>adj,boolean [] vis)
+    {
+        vis[v]=true;
+        for(int ele : adj.get(v))
+        {
+            if(vis[ele]==false)
+            {
+                dfs(ele,adj,vis);
+            }
+        }
+        
+    }
+    
+}
