07e92e6bdf3392c448c2194c3a81145945d1ff96
PIYUSH SINGH <96883232+Piyush1717@users.noreply.github.com>
2022-07-05 11:55:24 +0530

    Added solution - LeetHub

---
+++ Possible Path - GFG/possible-path.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] paths = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++){
+                    paths[i][j] = Integer.parseInt(s[j]);
+                }
+            }
+            Solution obj = new Solution();
+            int ans = obj.isPossible(paths);
+            System.out.println(ans);
+
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    public int isPossible(int[][] paths)
+    {
+        //we have to find whether its a euler circuit or not
+        
+        int count=0;
+        
+        for(int i=0;i<paths.length;i++)
+        {
+            int degree=0;
+            
+            for(int j=0;j<paths[0].length;j++)
+            {
+                if(paths[i][j]==1)
+                {
+                    degree++;
+                }
+            }
+            
+            if(degree%2==1)
+            {
+                count++;
+            }
+        }
+        
+        if(count==0)
+        {
+            return 1;
+        }
+        
+        return 0;
+        
+    }
+}
