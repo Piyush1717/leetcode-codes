4c8ac38098adcbc4ec46244123f8cf896e0beb68
PIYUSH SINGH <96883232+Piyush1717@users.noreply.github.com>
2022-07-14 14:35:24 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -0,0 +1,69 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+       int [] dp=new int [W+1];
+       
+       dp[0]=0;
+       
+       for(int bag=1;bag<dp.length;bag++)
+       {
+           int max=0;
+           for(int i=0;i<wt.length;i++)
+           {
+               if(bag-wt[i]>=0)
+               {
+                   int remain=bag-wt[i];
+                   int optRemain=dp[remain];
+                   int myVal=optRemain+val[i];
+                   
+                   if(myVal>max)
+                   {
+                       max=myVal;
+                   }
+                   
+               }
+               
+               
+           }
+           dp[bag]=max;
+           
+           
+       }
+       
+       return dp[W];
+       
+    }
+}
