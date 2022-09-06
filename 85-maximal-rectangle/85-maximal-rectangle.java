eca208399c9ba731b62999d8a190c54089714197
PIYUSH SINGH <96883232+Piyush1717@users.noreply.github.com>
2022-09-06 10:06:41 +0530

    Time: 62 ms (32.79%), Space: 53.9 MB (77.18%) - LeetHub

---
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -0,0 +1,107 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        
+        int [] ht=new int [matrix[0].length];
+        
+        for(int j=0;j<matrix[0].length;j++)
+        {
+            ht[j]=matrix[0][j]=='0'?0:1;
+        }
+        
+        int res=largestRectangleArea(ht);
+        
+        for(int i=1;i<matrix.length;i++){
+         
+        for(int j=0;j<matrix[0].length;j++)
+        {
+            ht[j]=matrix[i][j]=='0'?0:ht[j]+1;
+        }   
+            
+            int res2=largestRectangleArea(ht);
+            
+            res=Math.max(res,res2);
+        }
+        
+        return res;
+        
+    }
+    
+    
+    
+    public int largestRectangleArea(int[] heights) {
+        
+        int [] left=nextSmallerToLeft(heights);
+          int [] right=nextSmallerToRight(heights);
+        
+        int max=0;
+        
+        for(int i=0;i<left.length;i++)
+        {
+            max=Math.max(max, heights[i] * (right[i]-left[i]-1));
+        }
+        
+        return max;
+    }
+    
+    public int [] nextSmallerToRight(int [] arr)
+    {
+        
+        
+        int [] ans=new int[arr.length];
+        
+        Arrays.fill(ans,arr.length);
+        
+        Stack<Integer> st=new Stack<>();
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            while(st.size()>0 && arr[st.peek()]>arr[i])
+            {
+                ans[st.peek()]=i;
+                st.pop();
+            }
+            
+            st.push(i);            
+        }
+        
+        
+        return ans;
+        
+    }
+    
+    
+    
+    public int [] nextSmallerToLeft(int [] arr)
+    {
+        int [] ans=new int[arr.length];
+        Stack<Integer> st=new Stack<>();
+        
+        Arrays.fill(ans,-1);
+        
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            while(st.size()>0 && arr[st.peek()]>arr[i])
+            {
+                ans[st.peek()]=i;
+                st.pop();
+            }
+            
+            st.push(i);            
+        }
+        
+        
+        return ans;
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
