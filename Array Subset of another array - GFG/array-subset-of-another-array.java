d078ab6bef3c11008ae4868e54d7c3ea99d026d7
PIYUSH SINGH <96883232+Piyush1717@users.noreply.github.com>
2023-06-03 12:43:47 +0530

    Added solution - LeetHub

---
+++ Array Subset of another array - GFG/array-subset-of-another-array.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t-->0)
+        {
+            StringTokenizer stt = new StringTokenizer(br.readLine());
+            
+            long n = Long.parseLong(stt.nextToken());
+            long m = Long.parseLong(stt.nextToken());
+            long a1[] = new long[(int)(n)];
+            long a2[] = new long[(int)(m)];
+            
+            
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a1[i] = Long.parseLong(inputLine[i]);
+            }
+            String inputLine1[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < m; i++) {
+                a2[i] = Long.parseLong(inputLine1[i]);
+            }
+            
+            
+            Compute obj = new Compute();
+            System.out.println(obj.isSubset( a1, a2, n, m));
+            
+        }
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    public String isSubset( long a1[], long a2[], long n, long m) {
+        
+        HashMap<Long,Integer> hm=new HashMap<>();
+        
+        for(Long ele: a1)
+        {
+            hm.put(ele, hm.getOrDefault(ele,0)+1);
+        }
+        
+        String ans;
+        
+        for(int i=0;i<m;i++)
+        {
+            if(hm.containsKey(a2[i])==true)
+            {
+                int val=hm.get(a2[i]);
+                val=val-1;
+                if(val==0)
+                {
+                    hm.remove(a2[i]);
+                }
+                else
+                {
+                    hm.put(a2[i],val);
+                }
+            }
+            else
+            {
+                ans="No";
+                return ans;
+                
+            }
+            
+        }
+        
+        ans="Yes";
+        return ans;
+        
+        
+    }
+}
